8ce0fc867e93ff10e63012b147e7343ce462ab22
Abhi Raj <ar958587@gmail.com>
2025-09-03 07:30:40 +0000

    is the error with database

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -34,7 +34,13 @@ async function dbConnect() {
       return mongoose;
     });
   }
-  cached.conn = await cached.promise;
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+  
   return cached.conn;
 }
 
