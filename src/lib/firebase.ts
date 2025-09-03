ed5eeeab9f6e3e340bf2b6aa28de45117e829066
Abhi Raj <ar958587@gmail.com>
2025-09-03 05:54:00 +0000

    where is auth/configuration

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,14 @@
-import { initializeApp, getApps } from 'firebase/app';
+import { initializeApp, getApps, FirebaseOptions } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
   projectId: "studenthub-yfnf9",
   appId: "1:230995809521:web:38b60a169495a454158550",
-  storageBucket: "studenthub-yfnf9.firebasestorage.app",
+  storageBucket: "studenthub-yfnf9.appspot.com",
   apiKey: "AIzaSyCdzPvOrXV_ZsIVqCQZNrDCyOonYBAoyxs",
   authDomain: "studenthub-yfnf9.firebaseapp.com",
   messagingSenderId: "230995809521",
-  measurementId: ""
 };
 
 // Initialize Firebase
