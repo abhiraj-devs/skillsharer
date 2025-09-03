8ee3d3bfa3912ed699cc49aef7b214410793e9c9
Abhi Raj <ar958587@gmail.com>
2025-09-03 05:45:45 +0000

    same issue is there any error in db

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: 'studenthub-yfnf9',
-  appId: '1:230995809521:web:38b60a169495a454158550',
-  storageBucket: 'studenthub-yfnf9.firebasestorage.app',
-  apiKey: 'AIzaSyCdzPvOrXV_ZsIVqCQZNrDCyOonYBAoyxs',
-  authDomain: 'studenthub-yfnf9.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '230995809521',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
