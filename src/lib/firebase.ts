97bba622d93ddae2adec6688e651ccd0c461cbb1
Abhi Raj <ar958587@gmail.com>
2025-09-03 06:09:59 +0000

    why still the authentication error ;)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 const firebaseConfig: FirebaseOptions = {
   projectId: 'studenthub-yfnf9',
   appId: '1:230995809521:web:38b60a169495a454158550',
-  storageBucket: 'studenthub-yfnf9.firebasestorage.app',
+  storageBucket: 'studenthub-yfnf9.appspot.com',
   apiKey: 'AIzaSyCdzPvOrXV_ZsIVqCQZNrDCyOonYBAoyxs',
   authDomain: 'studenthub-yfnf9.firebaseapp.com',
   messagingSenderId: '230995809521',
